0b133bfd1a179777a6c95df6bedcf1dc350316ab
ilhamgurbanly10 <Ilham_gurbanly@mail.ru>
2022-06-06 10:22:33 +0500

    first commit

---
+++ js/slider.js
@@ -0,0 +1,19 @@
+
+flashSimpleSlider(".slider", {
+	dots: true,
+	buttons: true,
+	indexes: true,
+	autoplay: true,
+	autoplaySpeed: "normal",
+	disableArrows: true
+});
+
+flashSimpleSlider(".slider-2", {
+	dots: true,
+	buttons: true,
+	indexes: true,
+	autoplay: true,
+	autoplaySpeed: "normal",
+	disableArrows: true,
+	draggable: false
+});
